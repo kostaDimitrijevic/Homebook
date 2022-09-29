f8333fd77fe92ff9ca0441896e55004da8ef299b
Kosta Dimitrijevic <kole.dimi99@gmail.com>
2022-09-29 16:20:15 +0200

    already friends

--- app/src/main/java/com/example/homebook/friends/FriendsFragment.java
+++ app/src/main/java/com/example/homebook/friends/FriendsFragment.java
@@ -134,6 +134,16 @@ public class FriendsFragment extends Fragment {
 
     private void sendRequest(String from, String to){
 
+        friendViewModel.getAcceptedFriends().observe(getViewLifecycleOwner(), friends -> {
+            for (Friend friend:
+                 friends) {
+                if(friend.getUsername().equals(to)){
+                    Toast.makeText(mainActivity, "You are already friends with" + to, Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+        });
+
         DatabaseReference reference = FirebaseDatabase.getInstance("https://homebook-e8d20-default-rtdb.europe-west1.firebasedatabase.app/").getReference("Users");
         reference.get().addOnCompleteListener(task -> {
             if (task.isSuccessful()) {
